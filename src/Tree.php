2b47fdc1cc00c5e11d525c420c881ebca6bf9503
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-08-25 18:39:44 +0200

    unit test the Tree structure

--- src/Tree.php
+++ src/Tree.php
@@ -3,7 +3,49 @@
 namespace TreeRecursion;
 
 class Tree {
-  public function returnTrue() {
-    return false;
+  private string $name;
+
+  private self $parent;
+  
+  /** self[] */
+  private array $children = [];
+
+  public function setName(string $name): self
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  public function setParent(self $parent): self
+  {
+    $this->parent = $parent;
+
+    return $this;
+  }
+
+  public function addChild(self $child): self
+  {
+    $this->children[] = $child;
+
+    return $this;
+  }
+
+  public function name(): string
+  {
+    return $this->name;
+  }
+
+  public function parent(): self
+  {
+    return $this->parent;
+  }
+
+  /**
+   * @return self[]
+   */
+  public function children(): array
+  {
+    return $this->children;
   }
 }
--- tests/unit/TreeTest.php
+++ tests/unit/TreeTest.php
@@ -7,7 +7,35 @@ use TreeRecursion\Tree;
 
 class GetUserUseCaseTest extends TestCase
 {
-  public function testAssertFalse() {
-    $this->assertFalse((new Tree())->returnTrue());
+  public function testName() {
+    $expectedName = 'Tree 1';
+    $actualName = (new Tree())->setName($expectedName)->name();
+    $this->assertSame(
+      $expectedName, 
+      $actualName
+    );
+  }
+
+  public function testParent() {
+    $expectedParent = (new Tree())->setName('Parent Tree 1');
+    $actualParent = (new Tree())->setParent($expectedParent)->parent();
+    $this->assertEquals(
+      $expectedParent, 
+      $actualParent
+    );
+  }
+
+  public function testChildren() {
+    $expectedChild = (new Tree())->setName('Child Tree 1');
+    $anotherExpectedChild = (new Tree())->setName('Child Tree 2');
+    $expectedChildren = [$expectedChild, $anotherExpectedChild];
+    $actualChildren = (new Tree())
+                              ->addChild($expectedChild)
+                              ->addChild($anotherExpectedChild)
+                              ->children();
+    $this->assertEquals(
+      $expectedChildren, 
+      $actualChildren
+    );
   }
 }
